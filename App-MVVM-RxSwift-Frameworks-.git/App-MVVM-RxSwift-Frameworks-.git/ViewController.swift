467bf60b9e125faf7c8a40840a234bb9f2f649e5
Александр Муклинов <dkan425@gmail.com>
2024-06-14 07:46:17 +0400

    Create project

---
+++ App-MVVM-RxSwift-Frameworks-.git/App-MVVM-RxSwift-Frameworks-.git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-MVVM-RxSwift-Frameworks-.git
+//
+//  Created by Александр Муклинов on 14.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
